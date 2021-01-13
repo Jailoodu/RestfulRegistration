2f548d1613d288e8c2e2b2ee8965319e0af29634
Jaison Loodu <jaison_loodu@yahoo.ca>
2021-01-13 18:58:17 -0500

    adding documentation

--- api/implementation.py
+++ api/implementation.py
@@ -8,7 +8,13 @@ firebase_admin.initialize_app(cred)
 # initialize a client which will be used to perform operations on the database
 firestore_db = firestore.client()
 
-# returns a list of all the users in the database
+"""
+    Returns a list of all the users in the database
+    Args:
+        None
+    Returns:
+        List of dictionaries
+"""
 def get_users_list():
     snapshots = list(firestore_db.collection(u'users').get())
     li = []
@@ -16,11 +22,23 @@ def get_users_list():
         li.append(snapshot.to_dict())
     return li
 
-# creates user document in the database
+"""
+    Creates a user document in the database
+    Args:
+        Dictionary - request body
+    Returns:
+        None
+"""
 def create_user(data):
     firestore_db.collection(u'users').add(data)
 
-# fetches user details from the database given id
+"""
+    Returns the user details from the database
+    Args:
+        String - user ID
+    Returns:
+        Dictionary
+"""
 def get_user(id):
     users_ref = firestore_db.collection(u'users')
     # create a custom query which uses the id provided
@@ -31,26 +49,51 @@ def get_user(id):
     else:
         return None
 
-# updates specified fields for a user in the database
+"""
+    Updates specified fields for a user
+    Args:
+        Dictionary - updated fields
+        String - user ID
+    Returns:
+        None
+"""
 def update_user(data, id):
     doc_id = get_firestore_id(id)
     user_ref = firestore_db.collection(u'users').document(doc_id)
     user_ref.update(data)
 
-# deletes user document from the database
+"""
+    Deletes a user from the database
+    Args:
+        String - user ID
+    Returns:
+        None
+"""
 def delete_user(id):
     doc_id = get_firestore_id(id)
     user_ref = firestore_db.collection(u'users').document(doc_id)
     user_ref.delete()
 
-# queries database for the document id corresponding to the user id
+"""
+    Queries database for the document ID
+    Args:
+        String - user ID
+    Returns:
+        String - document ID
+"""
 def get_firestore_id(user_id):
     users_ref = firestore_db.collection(u'users')
     query = users_ref.where(u'id', u'==', user_id)
     docs = query.get()
     return docs[0].id
 
-# normalize input json for csv conversion
+"""
+    Data normalization for CSV conversion
+    Args:
+        List of dictionaries
+    Returns:
+        List
+"""
 def normalize(data):
     newData = pd.json_normalize(data)
     return newData
