ca264ebba623b8a6c5c15c933eae2a2881d412ae
Jaison Loodu <jaison_loodu@yahoo.ca>
2021-03-29 19:32:31 -0400

    Testing

--- app/app.py
+++ app/app.py
@@ -7,16 +7,13 @@ from flask_cors import CORS
 
 # initialize a Flash (WSGI) application
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.after_request
-def apply_caching(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+bp = Blueprint('api', __name__, url_prefix='/api')
 
 def start_app(app):
     # the endpoint will be hostname:port/api
-    bp = Blueprint('api', __name__, url_prefix='/api')
     api.init_app(bp)
     api.add_namespace(ns)
     # blueprint helps organize a group of related views
@@ -25,6 +22,12 @@ def start_app(app):
 def main():
     start_app(app)
     app.run(host ='0.0.0.0', port = 5001)
+
+@bp.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
 
 # main function is automatically run
 if __name__ == '__main__':
